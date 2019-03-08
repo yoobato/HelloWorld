6ead365762726ab9afee95308f728926874e3976
Daeyeol Ryu <dy@yoobato.com>
2019-03-08 18:41:09 +0900

    Add Django REST framework

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo'
+    'corsheaders',
+    'rest_framework',
+    'todo',
 ]
 
 MIDDLEWARE = [
