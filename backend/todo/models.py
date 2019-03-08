b572dff8648c4e03c03756594445198a4887fcaf
Daeyeol Ryu <dy@yoobato.com>
2019-03-08 18:30:54 +0900

    Add Todo model

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        if len(self.description) > 80 - 3:
+            return self.description[:77] + '...'
+        else:
+            return self.description
