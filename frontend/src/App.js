19fe903ae9b48d1b6f1d0a0f175d517da202d902
Daeyeol Ryu <dy@yoobato.com>
2019-03-13 02:26:28 +0900

    일단 테스트 데이터로 조직도 차트 적용

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,144 +1,69 @@
 import React, { Component } from 'react';
-import Modal from './components/Modal';
+import Chart from 'react-google-charts';
 import axios from 'axios';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            modal: false,
-            viewCompleted: false,
-            activeItem: {
-                description: "",
-                completed: false
-            },
-            todoList: []
+            memberList: [],
+            chartData: [['Name', 'Manager ID']]
         };
     };
+
     componentDidMount() {
         this.refreshList();
     }
 
     refreshList = () => {
-        axios
-            .get('http://localhost:8000/api/todos/')
-            .then(res => this.setState({ todoList: res.data }))
+        axios.get('http://localhost:8000/api/members/')
+            .then(res => this.setState({ memberList: res.data }))
             .catch(err => console.log(err));
+        this.convertMemberListToChartData()
+    };
+    convertMemberListToChartData = () => {
+        // TODO: MemberList -> ChartData 로 변환
+
+        this.setState({
+            chartData: [
+                        ['Name', 'Manager ID'],
+                        [
+                            {
+                                v: 'Mike',
+                                f: 'Mike<div style="color:red; font-style:italic">President</div>',
+                            },
+                            ''
+                        ],
+                        [
+                            {
+                                v: 'Jim',
+                                f: 'Jim<div style="color:red; font-style:italic">Vice President</div>',
+                            },
+                            'Mike',
+                        ],
+                        ['Alice', 'Mike'],
+                        ['Bob', 'Jim'],
+                        ['Carol', 'Bob'],
+                    ]
+        });
     };
 
-    toggle = () => {
-        this.setState({ modal: !this.state.modal });
-    };
-    handleSubmit = item => {
-        this.toggle();
-        if (item.id) {
-            axios
-                .put(`http://localhost:8000/api/todos/${item.id}/`, item)
-                .then(res => this.refreshList());
-        } else {
-            axios
-                .post('http://localhost:8000/api/todos/', item)
-                .then(res => this.refreshList());
-        }
-    };
-    handleDelete = item => {
-        axios
-            .delete(`http://localhost:8000/api/todos/${item.id}/`)
-            .then(res => this.refreshList());
-    };
-    createItem = () => {
-        const item = { description: "", completed: false };
-        this.setState({ activeItem: item, modal: !this.state.modal });
-    };
-    editItem = item => {
-        this.setState({ activeItem: item, modal: !this.state.modal });
-    };
-
-    displayCompleted = status => {
-        if (status) {
-            return this.setState({ viewCompleted: true });
-        }
-        return this.setState({ viewCompleted: false });
-    };
-
-    renderTabList = () => {
-        return (
-            <div className="my-5 tab-list">
-                <span
-                    onClick={() => this.displayCompleted(true)}
-                    className={this.state.viewCompleted ? "active" : ""}
-                >
-                    Complete
-                </span>
-                <span
-                    onClick={() => this.displayCompleted(false)}
-                    className={this.state.viewCompleted ? "" : "active"}
-                >
-                    Incomplete
-                </span>
-            </div>
-        );
-    };
-    renderItems = () => {
-        const { viewCompleted } = this.state;
-        const newItems = this.state.todoList.filter(
-            item => item.completed === viewCompleted
-        );
-        return newItems.map(item => (
-            <li
-                key={item.id}
-                className="list-group-item d-flex justify-content-between align-items-center"
-            >
-                <span className={`todo-summary mr-2 ${this.state.viewCompleted ? "completed-todo" : ""}`}>
-                    {item.description.length > 57 ? item.description.substr(0, 57) + '...' : item.description}
-                </span>
-                <span>
-                    <button
-                        onClick={() => this.editItem(item)}
-                        className="btn btn-secondary mr-2"
-                    >
-                        {" "}
-                        Edit{" "}
-                    </button>
-                    <button
-                        onClick={() => this.handleDelete(item)}
-                        className="btn btn-danger"
-                    >
-                        Delete{" "}
-                    </button>
-                </span>
-            </li>
-        ));
-    };
     render() {
         return (
             <main className="content">
-                <h1 className="text-white text-uppercase text-center my-4">Todo app</h1>
-                <div className="row">
-                    <div className="col-md-6 col-sm-10 mx-auto p-0">
-                        <div className="card p-3">
-                            <div>
-                                <button
-                                    onClick={this.createItem}
-                                    className="btn btn-primary"
-                                >
-                                    Add Todo
-                                </button>
-                            </div>
-                            {this.renderTabList()}
-                            <ul className="list-group list-group-flush">
-                                {this.renderItems()}
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                {this.state.modal ? (
-                    <Modal
-                        activeItem={this.state.activeItem}
-                        toggle={this.toggle}
-                        onSave={this.handleSubmit}
-                    />
-                ) : null}
+                <h1 className="chart-title">얼리브 크루 조직도</h1>
+
+                <Chart
+                    width={'100%'}
+                    height={350}
+                    chartType="OrgChart"
+                    loader={<div>조직도를 로딩하는 중입니다...</div>}
+                    data={this.state.chartData}
+                    options={{
+                        allowHtml: true,
+                    }}
+                    rootProps={{ 'data-testid': '1' }}
+                />
             </main>
         );
     };
