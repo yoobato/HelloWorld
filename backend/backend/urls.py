4f9d5962bb16a971d899d6873d7a40bb72ccb2cc
Daeyeol Ryu <dy@yoobato.com>
2019-03-12 14:51:51 +0900

    TODO backend 제거 // Organization API 추가

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from todo import views
+from organization import views
 
 
 router = routers.DefaultRouter()
-router.register(r'todos', views.TodoView, 'todo')
+router.register('organization/members', views.MemberView, 'member')
 
 
 urlpatterns = [
